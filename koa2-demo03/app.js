79386e09cd3b4d5b5d52c334090590eb1ab1d932
zhaojianxin <zhaojianxin@gozap.com>
2017-11-03 18:50:39 +0800

    koa demo

---
+++ koa2-demo03/app.js
@@ -0,0 +1,45 @@
+const nunjucks = require('nunjucks');
+
+function createEnv(path, opts) {
+    var
+        autoescape = opts.autoescape === undefined ? true : opts.autoescape,
+        noCache = opts.noCache || false,
+        watch = opts.watch || false,
+        throwOnUndefined = opts.throwOnUndefined || false,
+        env = new nunjucks.Environment(
+            new nunjucks.FileSystemLoader(path, {
+                noCache: noCache,
+                watch: watch,
+            }), {
+                autoescape: autoescape,
+                throwOnUndefined: throwOnUndefined
+            });
+    if (opts.filters) {
+        for (var f in opts.filters) {
+            env.addFilter(f, opts.filters[f]);
+        }
+    }
+    return env;
+}
+
+var env = createEnv('views', {
+    watch: true,
+    filters: {
+        hex: function (n) {
+            return '0x' + n.toString(16);
+        }
+    }
+});
+
+var s = env.render('hello.html', {
+    name: '<Nunjucks>',
+    fruits: ['Apple', 'Pear', 'Banana'],
+    count: 12000
+});
+
+console.log(s);
+
+console.log(env.render('extend.html', {
+    header: 'Hello',
+    body: 'bla bla bla...'
+}));
